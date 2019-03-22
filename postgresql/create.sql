f28a7eedea2ecdbc3c861c761a90af4aeb9e6727
rex897 <kostya77797@gmail.com>
2019-03-23 00:31:30 +0500

    удалил лишнее

--- postgresql/create.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE alfresco WITH LOGIN PASSWORD 'admin';
-CREATE DATABASE alfresco;
-GRANT ALL PRIVILEGES ON DATABASE alfresco TO alfresco;
-CREATE DATABASE notifications WITH OWNER = alfresco;
-CREATE DATABASE reporting WITH OWNER = alfresco;
